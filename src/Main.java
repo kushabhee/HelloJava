b6bd2dd01c4926f6ac5b357fa7e39cacdda02577
Your Name <kushabhee@icloud.com>
2024-12-31 22:39:29 +0545

    Initial commit for HelloJava

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello java!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    // Recursive method to solve the Tower of Hanoi
+    public static void solveHanoi(int n, char source, char destination, char auxiliary) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
         }
+        // Move n-1 disks from source to auxiliary
+        solveHanoi(n - 1, source, auxiliary, destination);
+
+        // Move the nth disk from source to destination
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+
+        // Move the n-1 disks from auxiliary to destination
+        solveHanoi(n - 1, auxiliary, destination, source);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Tower of Hanoi game!");
+        System.out.print("Enter the number of disks: ");
+        int numberOfDisks = scanner.nextInt();
+
+        // Validate input
+        if (numberOfDisks <= 0) {
+            System.out.println("The number of disks must be greater than 0.");
+            return;
+        }
+
+        System.out.println("Solving Tower of Hanoi with " + numberOfDisks + " disks...");
+        solveHanoi(numberOfDisks, 'A', 'C', 'B'); // A = Source, C = Destination, B = Auxiliary
+        System.out.println("Tower of Hanoi solved!");
     }
 }
